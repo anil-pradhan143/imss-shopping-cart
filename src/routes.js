0eccc364b544660a201090400d2266cfed0eddf9
anil.pradhan <anil.pradhan@innoviti.com>
2020-08-22 04:25:20 +0530

    #beneficiary task done

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Cart from './pages/Cart';
+import Beneficiary from './pages/Beneficiary';
 
 export default function Routes() {
     return (
         <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/cart" component={Cart} />
+            <Route path="/beneficiary" component={Beneficiary} />
         </Switch>
     );
 }
